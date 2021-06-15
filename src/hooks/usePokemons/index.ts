05aacbb96f38ada955bd39cf9d0d2d194501f1ce
Vagner S <vagners.tambalo@gmail.com>
2021-06-15 15:52:42 -0300

    feat(hooks): add paginated ids request to usePokemons

--- src/hooks/usePokemons/index.ts
+++ src/hooks/usePokemons/index.ts
@@ -1,4 +1,8 @@
-import { useQuery, UseQueryResult, UseBaseQueryOptions } from 'react-query'
+import {
+  useInfiniteQuery,
+  UseInfiniteQueryOptions,
+  UseInfiniteQueryResult,
+} from 'react-query'
 import { gql } from 'graphql-request'
 
 import client, { getPokemonPictureUrl } from '../../utils/api/client'
@@ -6,30 +10,104 @@ import client, { getPokemonPictureUrl } from '../../utils/api/client'
 import type { PokemonType } from '../../types/pokemon'
 
 type PokemonsResponse = {
-  pokemons: { id: number, name: string }[]
+  pokemons: { id: number, name: string }[],
 }
 
-const pokemonsQuery = gql`
-query ($ids: [Int!]) {
-  pokemons: pokemon_v2_pokemon(offset: 0, limit: 10, where: {id: {_in: $ids}}) {
-    id
-    name
+type QueryParams = {
+  offset?: number,
+  name?: string,
+  limit?: number,
+  ids?: number[],
+}
+
+type FetchResult = {
+  offset: number,
+  data: PokemonType[],
+}
+
+const PokemonInfo = gql`
+fragment PokemonInfo on pokemon_v2_pokemon {
+  id
+  name
+  types: pokemon_v2_pokemontypes {
+    slot
+    info: pokemon_v2_type {
+      name
+    }
   }
 }
 `
 
-const fetch = async (ids: number[]): Promise<PokemonType[]> => {
-  const { pokemons } = await client<PokemonsResponse, { ids: number[] }>(
-    pokemonsQuery, { ids }
-  )
+const pokemonsById = gql`
+${PokemonInfo}
 
-  return pokemons.map((pokemon) => ({
+query ($offset:  Int!, $limit: Int!, $ids: [Int!]) {
+  pokemons: pokemon_v2_pokemon(
+    offset: $offset,
+    limit: $limit,
+    where: { id: { _in: $ids } }
+  ) {
+    ...PokemonInfo
+  }
+}
+`
+
+const pokemonsByName = gql`
+${PokemonInfo}
+
+query ($offset: Int!, $limit: Int!, $name: String!) {
+  pokemons: pokemon_v2_pokemon(
+    offset: $offset,
+    limit: $limit,
+    where: { name: { _regex: $name } }
+  ) {
+    ...PokemonInfo
+  }
+}
+`
+
+const getPokemonsById = ({ ids, limit, offset }: QueryParams):
+  Promise<PokemonsResponse> => client(pokemonsById, { ids, limit, offset })
+
+const getPokemonsByName = ({ limit, name, offset }: QueryParams):
+  Promise<PokemonsResponse> => client(pokemonsByName, { limit, name, offset })
+
+const fetch = async (
+  { ids = [], limit = 10, name = '', offset = 0 }: QueryParams
+): Promise<FetchResult> => {
+  const request = ids.length > 0
+    ? getPokemonsById
+    : getPokemonsByName
+
+  const response = await request({ offset, name, limit, ids })
+
+  const pokemons = response.pokemons.map(pokemon => ({
     ...pokemon,
     picture: getPokemonPictureUrl(pokemon.id),
   }))
+
+  return {
+    offset: offset + limit,
+    data: pokemons,
+  }
 }
 
-const query = (ids: number[], opts: UseBaseQueryOptions<PokemonType[]>):
-  UseQueryResult<PokemonType[]> => useQuery('pokemonList', () => fetch(ids), opts)
+export type usePokemonsType = (
+  name: string,
+  ids: number[],
+  opts?: UseInfiniteQueryOptions<FetchResult>,
+) => Partial<UseInfiniteQueryResult<FetchResult>>
+
+const query: usePokemonsType = (
+  name, ids, opts,
+) => useInfiniteQuery(
+    ['pokemons', name, ids],
+    ({ pageParam }) => fetch({ offset: pageParam, name, ids }),
+    {
+      ...opts,
+      keepPreviousData: true,
+      getNextPageParam: ({ offset }: { offset: number }) => offset,
+    }
+  )
 
 export default query
