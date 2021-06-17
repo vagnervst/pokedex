ca5ed3574a87ca581172feff24f93f8b94cc9c62
Vagner S <vagners.tambalo@gmail.com>
2021-06-17 11:17:38 -0300

    feat(requests): add retry as false to react query defaults

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,7 @@ import App from './App'
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      retry: false,
       useErrorBoundary: true,
       refetchOnWindowFocus: false,
       refetchOnMount: false,
